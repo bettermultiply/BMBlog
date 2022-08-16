5714528a48c513dc7274d5fe8f5341f7a74ea555
BetterMultiply <bettermultiply@gamil.com>
2022-08-16 13:51:07 +0800

    prerender

--- pages/_app.js
+++ pages/_app.js
@@ -4,5 +4,9 @@ import Link from 'next/link';
 import { Component } from 'react';
 
 export default function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+    <div className='M'>
+        <Component {...pageProps} />
+    </div>
+    )
 }
